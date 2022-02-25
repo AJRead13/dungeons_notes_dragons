359f0bf9667fca26da20824f5160426edf24e326
Andrew Read <aj.read13@gmail.com>
2022-02-25 11:37:45 -0500

    working on pages

--- client/src/pages/CharSheetViewer.js
+++ client/src/pages/CharSheetViewer.js
@@ -1,7 +1,11 @@
 import React from 'react';
-
+import { Redirect, userParams } from 'react-router-dom';
+import { useQuery } from '@apollo/client';
+import Auth from '../utils/auth';
+import { Link } from 'react-router-dom'
 import CharSheet from '../components/CharSheet';
 import Searchbox from '../components/Searchbox';
+
 
 const CharSheetViewer = () => {
   return (
--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,14 +1,41 @@
 import React from 'react';
-
+import { Redirect, userParams } from 'react-router-dom';
+import { useQuery } from '@apollo/client';
+import Auth from '../utils/auth';
+import { Link } from 'react-router-dom'
 import CharSheet from '../components/CharSheet';
 import Searchbox from '../components/Searchbox';
+import { QUERY_USER, GET_Me} from '../utils/queries'
 
 const Profile = () => {
-  return (
-    <>
+  const { username: userParam} = userParams();
+  const { loading, data } = useQuery(userParam ? QUERY_USER : GET_ME, {
+    variables: { username: userParam },
+  });
+  const user = data?.me || data?.user || {};
+  if (Auth.loggedIn() && Auth.getProfile().data.username === userParam) {
+    return <Redirect to="/me"/>;
+  }
 
-    </>
-  )
-}
+  if (loading) {
+    return <div>Loading...</div>
+  }
+  if (!user?.username) {
+    return (
+      <h4>
+        You must be signed in to view the historical documents.
+        Please use the navigation items to sign up or log in.
+      </h4>
+    );
+  }
+
+  return (
+    <div>
+
+    </div>
+  );
+};
+  
+  
 
 export default Profile;
