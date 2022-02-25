64869d8f2bab8718bab7f63588df9c33fe4d54f6
Andrew Read <aj.read13@gmail.com>
2022-02-25 10:34:23 -0500

    updated jwt

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -9,7 +9,7 @@ module.exports = {
   // function for our authenticated routes
   authMiddleware: function (req, res, next) {
     // allows token to be sent via  req.query or headers
-    let token = req.query.token || req.headers.authorization;
+    let token = req.query.token || req.headers.authorization || req.body.token;
 
     // ["Bearer", "<tokenvalue>"]
     if (req.headers.authorization) {
