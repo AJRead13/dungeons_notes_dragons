1fd1569509198d8fd209fb0e1cbb00a3039ad98f
Tina Stanczyk <t9.stanczyk@gmail.com>
2022-02-25 13:16:47 -0500

    fixing resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,4 +1,6 @@
+const { AuthenticationError } = require('apollo-server-express');
 const { User, Character, Note } = require('../models');
+const { signToken } = require('../utils/auth');
 
 const resolvers = {
   Query: {
