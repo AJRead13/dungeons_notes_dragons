8707086b2c03d73fd044c820968f8f11914a43b8
Andrew Read <aj.read13@gmail.com>
2022-02-28 22:02:57 -0500

    updated tab title and charsheet components

--- client/src/components/CharSheet.js
+++ client/src/components/CharSheet.js
@@ -1,37 +1,7 @@
 import React from "react";
 
 const CharSheet = (character) => {
-  return (
-    <div class="charsheet">
-      <p>Name: {character.name}</p>
-      <p>Race: {character.race}</p>
-      <p>Class: {character.className}</p>
-      <p>Hit Points: {character.hitPoints}</p>
-      <p>
-        Strength: {character.str}/{character.strmod}
-      </p>
-      <p>
-        Dexterity: {character.dex}/{character.dexmod}
-      </p>
-      <p>
-        Constitution: {character.con}/{character.conmod}
-      </p>
-      <p>
-        Intelligence: {character.int}/{character.intmod}
-      </p>
-      <p>
-        Wisdom: {character.wis}/{character.wismod}
-      </p>
-      <p>
-        Charisma: {character.cha}/{character.chamod}
-      </p>
-    </div>
-  );
-};
-
-export default CharSheet;
-
-<div className="container">
+  return <div className="container">
   <div className="charInfo">
     <div className="col-4">
       <h3 className="charName">{character.name}</h3>
@@ -125,3 +95,36 @@ export default CharSheet;
     </div>
   </div>
 </div>;
+};
+
+export default CharSheet;
+
+
+
+
+// (
+//   <div class="charsheet">
+//     <p>Name: {character.name}</p>
+//     <p>Race: {character.race}</p>
+//     <p>Class: {character.className}</p>
+//     <p>Hit Points: {character.hitPoints}</p>
+//     <p>
+//       Strength: {character.str}/{character.strmod}
+//     </p>
+//     <p>
+//       Dexterity: {character.dex}/{character.dexmod}
+//     </p>
+//     <p>
+//       Constitution: {character.con}/{character.conmod}
+//     </p>
+//     <p>
+//       Intelligence: {character.int}/{character.intmod}
+//     </p>
+//     <p>
+//       Wisdom: {character.wis}/{character.wismod}
+//     </p>
+//     <p>
+//       Charisma: {character.cha}/{character.chamod}
+//     </p>
+//   </div>
+// );
